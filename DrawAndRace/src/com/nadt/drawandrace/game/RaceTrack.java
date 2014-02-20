5d11a8bc6fc2a07d4f2c4b3c81e80007647df51b
loclamor <favier.loic.31@gmail.com>
2014-02-20 16:54:46 +0100

    Add black and white bitmap in raceTrack

--- DrawAndRace/src/com/nadt/drawandrace/game/RaceTrack.java
+++ DrawAndRace/src/com/nadt/drawandrace/game/RaceTrack.java
@@ -1,5 +1,10 @@
 package com.nadt.drawandrace.game;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+
+import com.nadt.drawandrace.utils.Constants;
 import com.nadt.drawandrace.utils.Tools;
 
 public class RaceTrack {
@@ -21,11 +26,14 @@ public class RaceTrack {
 			{false, true,  true,  true,  true,  true,  true,  true,  false, false},
 			{false, false, false, false, false, false, false, false, false, false}
 	};
+	private Bitmap blackAndWhiteBitmap;
 	
 	public RaceTrack(int tileSize, int nbHorizontal, int nbVertical) {
 		wallSize = tileSize;
 		nbH = nbHorizontal;
 		nbV = nbVertical;
+		
+		blackAndWhiteBitmap = BitmapFactory.decodeFile( Constants.getBlackWhiteFilePath() );
 	}
 	
 	public boolean collisionOn(int x, int y) {
@@ -33,6 +41,14 @@ public class RaceTrack {
 //		int yInAray = Math.min(Math.max(y / wallSize, 0), nbV) ;
 //		Tools.log(this, "Test if there is obstacle in : [ " + xInAray + " , " + yInAray + "]");
 //		return !map[yInAray][xInAray];
+		
+		float litleWidth = blackAndWhiteBitmap.getWidth() / Constants.TRACK_SIZE_FACTOR;
+		float litleHeight = blackAndWhiteBitmap.getHeight() / Constants.TRACK_SIZE_FACTOR;
+		
+		if( blackAndWhiteBitmap.getPixel(x / Constants.TRACK_SIZE_FACTOR, y / Constants.TRACK_SIZE_FACTOR ) == Color.BLACK ) {
+			Tools.err(this, "*** BOOM ***");
+		}
+		
 		return false;
 	}
 
