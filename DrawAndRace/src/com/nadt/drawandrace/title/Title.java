021b62e4a5a779daf5253d43766733d9b14aa72e
Florian CLAVEL <florian.clavel@live.Fr>
2014-02-20 08:51:59 +0100

    Push classic activity

--- DrawAndRace/src/com/nadt/drawandrace/title/Title.java
+++ DrawAndRace/src/com/nadt/drawandrace/title/Title.java
@@ -2,15 +2,17 @@ package com.nadt.drawandrace.title;
 
 import java.io.File;
 
+import com.nadt.drawandrace.CustomActivity;
+import com.nadt.drawandrace.game.GameActivity;
+import com.nadt.drawnandrace.R;
+
 
 import android.content.Intent;
 import android.database.Cursor;
 
-import com.nadt.drawandrace.CustomActivity;
-import com.nadt.drawandrace.utils.Constants;
-import com.nadt.drawandrace.utils.Tools;
-
+import android.opengl.Visibility;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,11 +23,61 @@ import android.widget.TextView;
 
 
 public class Title extends CustomActivity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		// Compute game path
+		String packageName = Title.this.getPackageName();
+		File externalPath = Environment.getExternalStorageDirectory();
+		File storage = new File(externalPath.getAbsolutePath() + "/Android/data/" + packageName + "/files");
+		if(storage == null) {
+			storage = getApplication().getFilesDir();
+		}
+
+		setContentView(R.layout.title_activity);
+		Button start = (Button) findViewById(R.id.playButton);
+		Button exit = (Button) findViewById(R.id.exitButton);
+
+		start.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {	
+				Intent myIntent = new Intent(Title.this, GameActivity.class);
+				startActivityForResult(myIntent, 0);
+			}
+		});
+
+		//		create.setOnClickListener(new OnClickListener() {
+		//			@Override
+		//			public void onClick(View v) {	
+		//				Constants.mode = Constants.Mode.CREATE;
+		//				Intent myIntent = new Intent(Title.this, MusicSelection.class);
+		//				startActivityForResult(myIntent, 0);
+		//			}
+		//		});
+		//		
+		//		scores.setOnClickListener(new OnClickListener() {
+		//			@Override
+		//			public void onClick(View v) {	
+		//				Intent myIntent = new Intent(Title.this, ScoreActivity.class);
+		//				startActivityForResult(myIntent, 0);
+		//			}
+		//		});
+		//		
+		//		options.setOnClickListener(new OnClickListener() {
+		//			@Override
+		//			public void onClick(View v) {	
+		//				Intent myIntent = new Intent(Title.this, Parameters.class);
+		//				startActivityForResult(myIntent, 0);
+		//			}
+		//		});
+
+		exit.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 	}
 
 	@Override
