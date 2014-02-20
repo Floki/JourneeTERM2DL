0f81190e5cbfe6d5f6cc2406104be0eaba5e0785
VincentCarassus <vincentcarassus@gmail.com>
2014-02-20 11:27:58 +0100

    Converting to B&W pixel by pixel

--- DrawAndRace/src/com/nadt/drawandrace/utils/ImageManipulation.java
+++ DrawAndRace/src/com/nadt/drawandrace/utils/ImageManipulation.java
@@ -6,13 +6,18 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 
-public class ImageManipulation {
+public final class ImageManipulation {
+	
+	public static int threshold = 127;
+	
+	/**
+	 * Private constructor to prevent class instanciation
+	 */
+	private ImageManipulation(){}
 	
 	/**
 	 * Returns a black and white (only black and white, no grey) bitmap of
 	 * the given bitmap
-	 * 
-	 * TODO : for now does a grayscale, change to do a real B&W 
 	 * 
 	 * @param bmpSrc
 	 * @return
@@ -20,16 +25,17 @@ public class ImageManipulation {
 	public static Bitmap toBlackAndWhite(Bitmap bmpSrc) {
 		int width, height;
 	    height = bmpSrc.getHeight();
-	    width = bmpSrc.getWidth();   
+	    width = bmpSrc.getWidth();
 	    
 	    Bitmap bmpBlackAndWhite = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-	    Canvas c = new Canvas(bmpBlackAndWhite);
-	    Paint paint = new Paint();
-	    ColorMatrix cm = new ColorMatrix();
-	    cm.setSaturation(0);
-	    ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-	    paint.setColorFilter(f);
-	    c.drawBitmap(bmpSrc, 0, 0, paint);
+	    int color = 0;
+	    for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
+				color = bmpSrc.getPixel(i, j) > threshold ? 0 : 255;
+				bmpBlackAndWhite.setPixel(i, j, color);
+			}
+		}
+	    
 	    return bmpBlackAndWhite;
 	}
 	
